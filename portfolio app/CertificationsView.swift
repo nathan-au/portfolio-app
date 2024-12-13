9edbfff6ef4a13c8f5065e94b2f6d402f95a49dc
nathan-au <im.nathanau@gmail.com>
2024-12-13 11:27:13 -0500

    git commit from xcode

--- portfolio app/CertificationsView.swift
+++ portfolio app/CertificationsView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct CertificationsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("here are my certys")
     }
 }
 
